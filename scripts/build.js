93039cb45ad0d33e3a7b3595e78593aafaca8c53
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2018-12-24 23:59:41 +0330

    support react-scripts 2.1.2

--- scripts/build.js
+++ scripts/build.js
@@ -1,18 +1,25 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
+require('react-scripts/config/env');
+
 const fs = require('fs-extra');
 const path = require('path');
-
-require('react-scripts/config/env');
 
 const chalk = require('chalk');
 const appPath = require('react-scripts/config/paths').appPath;
 const webpackMonkeyPath = path.resolve(appPath, 'webpack.monkey.js');
-const webpackConfig = require('react-scripts/config/webpack.config.prod');
+
+const originalConfigFactory = require('react-scripts/config/webpack.config');
 
 if (fs.existsSync(webpackMonkeyPath)) {
     console.log(chalk.yellow('WARNING! You are using modified webpack config!'));
-    require(webpackMonkeyPath)(webpackConfig, false);
+    const configPatch = require(webpackMonkeyPath)
+    const configModule = require.cache[require.resolve('react-scripts/config/webpack.config')];
+    configModule.exports = (...args) => {
+        const webpackConfig = originalConfigFactory(...args);
+        return configPatch(webpackConfig, false) || webpackConfig;
+    };
 }
+
 require('react-scripts/scripts/build');
--- scripts/start.js
+++ scripts/start.js
@@ -1,18 +1,25 @@
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
-
-const fs = require('fs');
-const path = require('path');
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
 
 require('react-scripts/config/env');
+
+const fs = require('fs-extra');
+const path = require('path');
 
 const chalk = require('chalk');
 const appPath = require('react-scripts/config/paths').appPath;
 const webpackMonkeyPath = path.resolve(appPath, 'webpack.monkey.js');
-const webpackConfig = require('react-scripts/config/webpack.config.dev');
+
+const originalConfigFactory = require('react-scripts/config/webpack.config');
 
 if (fs.existsSync(webpackMonkeyPath)) {
     console.log(chalk.yellow('WARNING! You are using modified webpack config!'));
-    require(webpackMonkeyPath)(webpackConfig, true);
+    const configPatch = require(webpackMonkeyPath)
+    const configModule = require.cache[require.resolve('react-scripts/config/webpack.config')];
+    configModule.exports = (...args) => {
+        const webpackConfig = originalConfigFactory(...args);
+        return configPatch(webpackConfig, true) || webpackConfig;
+    };
 }
+
 require('react-scripts/scripts/start');
