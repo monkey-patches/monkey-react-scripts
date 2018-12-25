2813f0bcc602e13e41ff4c692aee4809b815ab14
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2018-12-25 11:24:36 +0330

    support .babelrc in the test command

--- scripts/test.js
+++ scripts/test.js
@@ -1,2 +1,28 @@
-// TODO need help for customization
+process.env.BABEL_ENV = 'test';
+process.env.NODE_ENV = 'test';
+process.env.PUBLIC_URL = '';
+
+require('react-scripts/config/env');
+
+const patchModule = require('./utils/cache');
+
+const fs = require('fs-extra');
+const path = require('path');
+
+const chalk = require('chalk');
+const appPath = require('react-scripts/config/paths').appPath;
+const webpackMonkeyPath = path.resolve(appPath, 'webpack.monkey.js');
+
+require('react-scripts/config/jest/babelTransform.js')
+
+if (fs.existsSync(webpackMonkeyPath)) {
+    console.log(chalk.yellow('WARNING! .babelrc file is enabled!'));
+    const babelJest = require('babel-jest');
+    patchModule('react-scripts/config/jest/babelTransform.js', babelJest.createTransformer({
+        presets: [require.resolve('babel-preset-react-app')],
+        babelrc: true,
+        configFile: false,
+    }));
+}
+
 require('react-scripts/scripts/test');
